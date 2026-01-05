afcb03358d1a61d09b703437aa520931613e73a2
nzommmo <ericnzomo17@gmail.com>
2026-01-05 22:12:38 +0300

    Update

--- career_canopy/settings.py
+++ career_canopy/settings.py
@@ -160,6 +160,8 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite dev server
+    "https://careercanopy.zearom.com/"
+
 ]
 CORS_ALLOW_CREDENTIALS = True
 
